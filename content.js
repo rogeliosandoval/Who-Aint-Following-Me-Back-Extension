048440614c0ba23f27664fabd753896c20b72618
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-10-18 10:26:00 -0500

    clean up for new version

--- content.js
+++ content.js
@@ -1,4 +1,1 @@
-// Right now this file doesn’t need to do much. This is just for what happens when you visit Instagram
-console.log('Instagram scraper content script loaded.')
-localStorage.removeItem('scrapedFollowers')
-localStorage.removeItem('scrapedFollowing')
+// Nothing to do here
